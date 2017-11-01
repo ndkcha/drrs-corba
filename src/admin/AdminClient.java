261ccd46f42f70cade2e7005074eb9edc9ca8d2d
Anand Kacha <ndkcha@gmail.com>
2017-10-31 20:51:33 -0400

    Client mechanism for creating room

---
+++ src/admin/AdminClient.java
@@ -0,0 +1,114 @@
+package admin;
+
+import RoomReservationApp.Campus;
+import RoomReservationApp.CampusHelper;
+import org.omg.CORBA.ORB;
+import org.omg.CORBA.ORBPackage.InvalidName;
+import org.omg.CORBA.Object;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.CosNaming.NamingContextPackage.CannotProceed;
+import org.omg.CosNaming.NamingContextPackage.NotFound;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+
+public class AdminClient {
+    public static void main(String args[]) {
+        boolean isExitRequested = false, success;
+        int choice;
+        String message;
+        Campus campus;
+        String campusNamingReference, adminId;
+        AdminOperations adminOps;
+        Logger logs = Logger.getLogger("Admin Client");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the (reference) name of the server: ");
+        campusNamingReference = scanner.nextLine();
+
+        // start orb client
+        try {
+            // create and initialize orb
+            ORB orb = ORB.init(args, null);
+
+            // get the root naming context
+            Object objectReference = orb.resolve_initial_references("NameService");
+            NamingContextExt ncRef = NamingContextExtHelper.narrow(objectReference);
+
+            // get the remote interface
+            campus = CampusHelper.narrow(ncRef.resolve_str(campusNamingReference));
+        } catch (InvalidName invalidName) {
+            logs.severe("Invalid reference to Name Service. \nMessage: " + invalidName.getMessage());
+            return;
+        } catch (CannotProceed cannotProceed) {
+            logs.severe("CannotProceed exception thrown. \nMessage: " + cannotProceed.getMessage());
+            return;
+        } catch (org.omg.CosNaming.NamingContextPackage.InvalidName invalidName) {
+            logs.severe("Invalid reference to the server. Please check the name. \n Message:" + invalidName.getMessage());
+            return;
+        } catch (NotFound notFound) {
+            logs.severe("Server not found.\nMessage: " + notFound.getMessage());
+            return;
+        }
+
+        // no remote. no work.
+        if (campus == null) {
+            logs.severe("Error initializing ORB object. Try again later!");
+            return;
+        }
+
+        // initialize implementation class for the client
+        adminOps = new AdminOperations(campus, logs);
+
+        // get adminId from the user
+        adminId = adminOps.askAdminId(scanner);
+
+        // lookup the adminId at server
+        if (!campus.lookupAdmin(adminId)) {
+            logs.warning("No entry found for the adminId, " + adminId);
+            return;
+        }
+
+        // set up file handler for logging mechanism.
+        try {
+            FileHandler handler = new FileHandler(adminId + ".log", true);
+            logs.addHandler(handler);
+        } catch (IOException ioe) {
+            logs.warning("Error initializing log file.\n Message: " + ioe.getMessage());
+        }
+
+        // be nice
+        System.out.print("\n\n\tWelcome to the campus!\n");
+
+        do {
+            // ask what to do
+            System.out.print("What do you want to do?\n\t1. Create a room\n\t2. Delete a room\nAny other number to exit");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                // create a room
+                case 1:
+                    success = adminOps.createRoom(scanner);
+                    message = success ? "A room has successfully been created." : "An unexpected error thrown while creating a room.";
+                    break;
+                // exit
+                default:
+                    message = "Exit requested! Have a nice day!";
+                    success = true;
+                    isExitRequested = true;
+                    break;
+            }
+
+            if (success)
+                logs.info(message);
+            else
+                logs.warning(message);
+        } while (!isExitRequested);
+
+        scanner.close();
+    }
+}
---
+++ src/admin/AdminOperations.java
@@ -0,0 +1,85 @@
+package admin;
+
+import RoomReservationApp.Campus;
+import RoomReservationApp.TimeSlot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+class AdminOperations {
+    private Campus campus;
+    private Logger logs;
+
+    AdminOperations(Campus campus, Logger logs) {
+        this.campus = campus;
+        this.logs = logs;
+    }
+
+    String askAdminId(Scanner scan) {
+        String response, adminId;
+
+        System.out.println("\nDo you have adminId? (y/n)");
+        response = scan.nextLine();
+
+        if (response.equalsIgnoreCase("y")) {
+            System.out.println("\nEnter your adminId: ");
+            adminId = scan.nextLine();
+        } else {
+            adminId = campus.generateAdminId();
+            System.out.println("\nYour new adminId is " + adminId + ".\nUse it for the logging into the system next time.");
+        }
+
+        return adminId;
+    }
+
+    boolean createRoom(Scanner scan) {
+        List<TimeSlot> timeSlots = new ArrayList<>();
+        int roomNo, noOfTimeSlots;
+        String date, start, end, response;
+        boolean isMoreSlots, success = false;
+
+        // ask necessary things
+        System.out.println("Enter the date (format: dd-MM-yyyy, e.g. 11-01-2018):");
+        date = scan.nextLine();
+
+        System.out.println("Enter the room number (integer values only):");
+        roomNo = scan.nextInt();
+        scan.nextLine();
+
+        System.out.println("Entries for the time-slots:");
+
+        do {
+            System.out.println("Enter the start time (hh:mm):");
+            start = scan.nextLine();
+
+            System.out.println("Enter the end time (hh:mm):");
+            end = scan.nextLine();
+
+            TimeSlot slot = new TimeSlot();
+            slot.startTime = start;
+            slot.endTime = end;
+            timeSlots.add(slot);
+
+            System.out.println("Add another time-slot (y/n):");
+            response = scan.nextLine();
+
+            isMoreSlots = response.equalsIgnoreCase("y");
+        } while (isMoreSlots);
+
+        // because of the marshalling in ORB architecture
+        noOfTimeSlots = timeSlots.size();
+        TimeSlot slots[] = new TimeSlot[noOfTimeSlots];
+
+        for (int i = 0; i < slots.length; i++) {
+            slots[i] = timeSlots.get(i);
+        }
+
+        // hit
+        success = campus.createRoom(date, roomNo, slots);
+
+        // respond
+        return success;
+    }
+}
