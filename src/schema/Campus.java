4315f08aa0005950d1f3dcfc2c5445de4bfd0c88
Anand Kacha <ndkcha@gmail.com>
2017-10-31 20:49:59 -0400

    Added data holder classes

---
+++ src/schema/Campus.java
@@ -0,0 +1,28 @@
+package schema;
+
+import java.io.Serializable;
+
+public class Campus implements Serializable {
+    private int udpPort;
+    private String namingReference, code;
+    public String name;
+
+    public Campus(int udpPort, String code, String namingReference, String name) {
+        this.udpPort = udpPort;
+        this.code = code;
+        this.namingReference = namingReference;
+        this.name = name;
+    }
+
+    public int getUdpPort() {
+        return udpPort;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getNamingReference() {
+        return namingReference;
+    }
+}
